fb2984fcc6b830f8ff2945f16e0bc75edc837b88
Никита <kaktusloco@gmail.com>
2023-03-13 17:02:57 +0300

    add table

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EditScheduleView from "@/views/EditScheduleView.vue";
 import ScheduleView from "@/views/ScheduleView.vue";
+import DiaryView from "@/views/DiaryView.vue";
+import JournalView from "@/views/JournalView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: "/edit-schedule",
       name: "edit-schedule",
       component: EditScheduleView,
+    },
+    {
+      path: "/diary",
+      name: "diary",
+      component: DiaryView,
+    },
+    {
+      path: "/journal",
+      name: "journal",
+      component: JournalView,
     },
   ],
 });
---
+++ frontend/src/views/DiaryView.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <DataTable
+      :value="items"
+      rowGroupMode="subheader"
+      groupRowsBy="subject"
+      scrollable
+      scrollHeight="90vh"
+      resizable-columns
+    >
+      <Column field="subject" header="subject" resizable />
+      <Column field="time" header="Время" resizable />
+      <Column field="date" header="Дата" resizable />
+      <Column field="note" header="Примечание" resizable />
+      <Column field="teacher" header="Проподаватель" resizable />
+      <Column field="mark" header="Оценка" resizable />
+      <template #groupheader="slotProps">
+        <div class="flex align-items-center gap-2">
+          <span>{{ slotProps.data.subject }}</span>
+        </div>
+      </template>
+    </DataTable>
+  </div>
+</template>
+
+<script>
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import ColumnGroup from "primevue/columngroup"; // optional
+import Row from "primevue/row"; // optional
+import InputText from "primevue/inputtext";
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+
+export default {
+  name: "DiaryView",
+  components: {
+    DataTable,
+    Column,
+    ColumnGroup,
+    Row,
+    InputText,
+  },
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          time: "12:50",
+          subject: "Математика",
+          note: "Самостоятельная работа",
+          teacher: "Морозова И А",
+          mark: "5",
+          date: "21.01.2023",
+        },
+        {
+          id: 2,
+          time: "12:50",
+          subject: "Математика",
+          note: "Самостоятельная работа",
+          teacher: "Морозова И А",
+          mark: "5",
+          date: "21.01.2023",
+        },
+        {
+          id: 3,
+          time: "12:50",
+          subject: "Математика",
+          note: "Самостоятельная работа",
+          teacher: "Морозова И А",
+          mark: "5",
+          date: "21.01.2023",
+        },
+        {
+          id: 4,
+          time: "12:50",
+          subject: "Математика",
+          note: "Самостоятельная работа",
+          teacher: "Морозова И А",
+          mark: "5",
+          date: "21.01.2023",
+        },
+        {
+          id: 5,
+          time: "12:50",
+          subject: "Математика",
+          note: "Самостоятельная работа",
+          teacher: "Морозова И А",
+          mark: "5",
+          date: "21.01.2023",
+        },
+        {
+          id: 6,
+          time: "12:50",
+          subject: "Русский язык",
+          note: "Самостоятельная работа",
+          teacher: "Морозова И А",
+          mark: "5",
+          date: "21.01.2023",
+        },
+        {
+          id: 7,
+          time: "12:50",
+          subject: "Русский язык",
+          note: "Самостоятельная работа",
+          teacher: "Морозова И А",
+          mark: "5",
+          date: "21.01.2023",
+        },
+        {
+          id: 8,
+          time: "12:50",
+          subject: "Русский язык",
+          note: "Самостоятельная работа",
+          teacher: "Морозова И А",
+          mark: "5",
+          date: "21.01.2023",
+        },
+        {
+          id: 9,
+          time: "12:50",
+          subject: "Русский язык",
+          note: "Самостоятельная работа",
+          teacher: "Морозова И А",
+          mark: "5",
+          date: "21.01.2023",
+        },
+        {
+          id: 10,
+          time: "12:50",
+          subject: "Русский язык",
+          note: "Самостоятельная работа",
+          teacher: "Морозова И А",
+          mark: "5",
+          date: "21.01.2023",
+        },
+        {
+          id: 11,
+          time: "12:50",
+          subject: "Русский язык",
+          note: "Самостоятельная работа",
+          teacher: "Морозова И А",
+          mark: "5",
+          date: "21.01.2023",
+        },
+      ],
+    };
+  },
+};
+</script>
